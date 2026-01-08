a3e188bec6879d34d44134fff37a92c682eadd5c
carlos19men <camendez.23@est.ucab.edu.ve>
2026-01-08 15:51:54 -0400

    fix: se corrigió la sección de tecnicos, ya está funcional

--- types/tecnicos.types.ts
+++ types/tecnicos.types.ts
@@ -4,6 +4,7 @@ export type Tecnico = {
   nombre?: string;
   correo?: string; // Opcional por si no viene de la API
   area?: string;
+  idGrupo?: number; // (solución temporal)
 };
 
 // Tipo para compatibilidad con el código existente
